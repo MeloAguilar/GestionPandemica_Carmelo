942abe321f850a41b68def0084134653cd0da00a
Melo Aguilar <carmelo.aguilar@iesnervion.es>
2022-05-18 13:57:56 +0200

    Agregar archivos de proyecto.

---
+++ Entities/clsPersona.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    public class clsPersona
+    {
+
+        [Required(ErrorMessage = "El Dni es un campo obligatorio")]
+        public int DniPersona { get; set; }
+
+        [MaxLength(20), Required(ErrorMessage = "Debe introducir su nombre")]
+        public string NombrePersona { get; set; }
+
+        [MaxLength(50), Required(ErrorMessage = "Debe introducir sus apellidos")]
+        public string  ApellidosPersona { get; set; }
+        [MaxLength(12)]
+        public string Telefono { get; set; }
+
+        [MaxLength(100)]
+        public string Direccion { get; set; }
+
+        
+        public clsPersona() { }
+
+        public clsPersona(int dniPersona, string nombrePersona, string apellidosPersona)
+        {
+            DniPersona=dniPersona;
+            NombrePersona=nombrePersona;
+            ApellidosPersona=apellidosPersona;
+        }
+    }
+}
---
+++ Entities/clsPregunta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class clsPregunta
+    {
+        public int IdPregunta { get; }
+
+        public string Pregunta { get; }
+
+
+        public clsPregunta() { }
+    }
+}
